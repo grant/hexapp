b6c6b119336a4b24453bf700d778f0f9b803dabc
Grant Timmerman <granttimmerman@gmail.com>
2014-10-01 21:07:40 -0700

    Add basic grid init code

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,33 @@
 $(function () {
 
+  var Hexgrid = require('hexgrid');
+  var Hexcell = require('hexcell');
+
+  // Globals
+  var grid;
+
+  // Greates the grid
+  function createGrid () {
+    grid = new Hexgrid();
+
+    grid.add(new Hexcell(new Point(0, 0)));
+    grid.add(new Hexcell(new Point(1, 0)));
+  }
+
+  // Renders the grid
+  function displayGrid () {
+    var cells = grid.getHexcells();
+    for (var i = 0; i < cells.length; ++i) {
+      var cell = cells[i];
+      var $cell = $('<div/>').addClass('cell');
+      $cell.attr('top', cell.point.y);
+      $cell.attr('left', cell.point.x);
+      $('.hex-grid').append($cell);
+    }
+  }
+
+  // Init
+  createGrid();
+  displayGrid();
+
 });
